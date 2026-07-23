dde8d69c9e415d7a3d11a7f7f7e99caa8786cd71
Njoku360 <frannjoku200@gmail.com>
2026-07-23 15:23:34 -0700

    added another kata

---
+++ lightsabers.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/51f9d93b4095e0a7200001b8
+// kata: How many lightsabers do you own?
+// My solution:
+function howManyLightsabersDoYouOwn(name) {
+  return name === 'Zach'? 18: 0
+}
