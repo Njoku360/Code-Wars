db240a678054a32798ca54b32dc719e0eb0f216f
Njoku360 <frannjoku200@gmail.com>
2026-07-23 15:11:59 -0700

    added a new kata

---
+++ Return_the_day.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/59dd3ccdded72fc78b000b25
+// kata: Return the day
+// My solution:
+function whatday(num) { 
+  // put your code here
+  if(num === 1){
+    return 'Sunday'
+  }else if(num === 2){
+    return 'Monday'
+  }else if(num === 3){
+    return 'Tuesday'
+  }else if(num === 4){
+    return 'Wednesday'
+  }else if(num === 5){
+    return 'Thursday'
+  }else if(num === 6){
+    return 'Friday'
+  }else if(num === 7){
+    return 'Saturday'
+  }else{
+    return 'Wrong, please enter a number between 1 and 7'
+  }
+}
