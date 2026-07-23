5c83097c529663b8f801cb685f8eb2eee3e02e7b
Njoku360 <frannjoku200@gmail.com>
2026-07-23 15:26:46 -0700

    added another kata

---
+++ Welcome.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5302d846be2a9189af0001e4
+// kata: Welcome to the City
+// My solution:
+function sayHello( name, city, state ) {
+  let arr = name.join(' ')
+  return `Hello, ${arr}! Welcome to ${city}, ${state}!`
+}
