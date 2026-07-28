f4c48106dfd2ecf40067e4e8241287894f7dbf7f
Njoku360 <frannjoku200@gmail.com>
2026-07-28 15:45:31 -0700

    added another kata

---
+++ Hello.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/523b4ff7adca849afe000035
+// kata: Function 1 - Hello world
+// My solution:
+//  Write a function "greet" that returns "hello world!"
+const greet = _ => 'hello world!'
