a06ccf7f8fded4d6f39393e979aba2e3e12331fd
Njoku360 <frannjoku200@gmail.com>
2026-07-31 15:59:58 -0700

    added

---
+++ Convert.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd
+// kata: Convert Boolean to a string
+// My solution:
+function booleanToString(b){
+  //your code here
+  return b ? 'true': 'false'
+}
