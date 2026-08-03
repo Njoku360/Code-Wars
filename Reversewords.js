a39e7f76cdf9c517123e44e59556017f973d7144
Njoku360 <frannjoku200@gmail.com>
2026-08-03 14:54:55 -0700

    added another kata

---
+++ Reversewords.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5259b20d6021e9e14c0010d4
+// kata: Reverse words
+// My solution:
+function reverseWords(str) {
+  // Go for it
+  return str.split(' ').map(word => word.split('').reverse().join('')).join(' ')
+}
---
+++ Stringend.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+// kata: Strings ends with?
+// My solution:
+let solution = function (str, ending){
+  return str.endsWith(ending)
+}
