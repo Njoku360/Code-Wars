e3cdbbf74c816f7bb661c15447303964959ea747
Njoku360 <frannjoku200@gmail.com>
2026-08-10 14:54:51 -0700

    added another kata

---
+++ arraysorting.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c
+// kata: Sort array by string length
+// My solution:
+const sortByLength = function (array) {return array.sort((a, b) => a.length - b.length)}
