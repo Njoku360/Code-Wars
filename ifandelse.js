ff1a8750e24808d00e8f90a37d4522318aa42534
Njoku360 <frannjoku200@gmail.com>
2026-08-11 15:54:59 -0700

    added another kata

---
+++ ifandelse.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57202aefe8d6c514300001fd
+// kata: if...else and ternary operator
+// My solution:
+function saleHotdogs(n){
+  if (n < 5) return n * 100
+  else if ( n >= 5 && n < 10) return n * 95
+  else return n * 90
+}
---
+++ listfiltering.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/53dbd5315a3c69eed20002dd
+// kata: List Filtering
+// My solution:
+const filter_list = (l) => l.filter(e => typeof e === 'number')
