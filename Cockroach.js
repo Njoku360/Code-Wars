299d2859a83d84d4231a05e1bb8d72a711964c66
Njoku360 <frannjoku200@gmail.com>
2026-08-27 15:29:01 -0700

    added another kata

---
+++ Cockroach.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6
+// kata: Beginner series#4 Cockroach
+// My solution
+function cockroachSpeed(s) {
+  //Good Luck!
+  return Math.floor(s * 100000/3600)
+}
