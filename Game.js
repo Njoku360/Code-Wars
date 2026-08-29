067ceaf7cc3a2ea85ad434df0144be9b7ecd3a24
Njoku360 <frannjoku200@gmail.com>
2026-08-29 14:20:58 -0700

    added another kata

---
+++ Game.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/523b4ff7adca849afe000035
+// kata: Grasshopper - Terminal Game move function
+// My solution:
+const move = (position, roll) => position + (roll * 2)
