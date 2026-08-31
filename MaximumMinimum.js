87677103203e694a9dce1c5e94bbb17aa402a928
Njoku360 <frannjoku200@gmail.com>
2026-08-31 15:32:56 -0700

    added another kata

---
+++ MaximumMinimum.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/577a98a6ae28071780000989
+// kata: Find Maximum and Minimum Values of a List 
+// My solution:
+var min = function(list){
+  return Math.min(...list)
+}
+
+var max = function(list){
+    return Math.max(...list)
+}
