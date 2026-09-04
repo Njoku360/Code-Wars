3e4d268b1597fbf1619e3ecbccc23aab73cebaac
Njoku360 <frannjoku200@gmail.com>
2026-09-04 15:23:28 -0700

    added another kata

---
+++ stringUppercase.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/56cd44e1aa4ac7879200010b
+// kata: Is the string uppercase
+// My solution:
+String.prototype.isUpperCase = function() {
+  // your code here
+  return this == this.toUpperCase()
+}
