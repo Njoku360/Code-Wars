4f1e97dd8d4bc3df97f42ca1ce1825aca3319086
Njoku360 <frannjoku200@gmail.com>
2026-09-12 15:33:56 -0700

    added another kata

---
+++ SuperEasy.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/55a5bfaa756cfede78000026
+// kata: Super Duper Easy
+// My solution:
+function problem(x){
+  //your code here
+  if(typeof x === 'string'){
+    return 'Error'
+  }
+  return (x * 50) + 6
+}
