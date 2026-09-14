340675a83dea518aa23fb07a4406433d96c0344c
Njoku360 <frannjoku200@gmail.com>
2026-09-14 15:51:02 -0700

    added another kata

---
+++ Messi.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55f73be6e12baaa5900000d4
+// kata: Grasshopper: Messi goals function
+// My solution:
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // code goes here
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
