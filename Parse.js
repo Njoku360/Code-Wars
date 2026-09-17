9226cf0c3ad1017712b2dea434d5078581962ab2
Njoku360 <frannjoku200@gmail.com>
2026-09-17 15:17:43 -0700

    added another kata

---
+++ Parse.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1
+// kata: Parse nice integer for character's problem
+// My solution:
+// return the girl's correct age as an integer. Happy coding
+const getAge = (inputString) => Number(inputString[0])
