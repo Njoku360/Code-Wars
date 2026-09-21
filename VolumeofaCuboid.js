90cc9411401686da19cea436200401532a5105b6
Njoku360 <frannjoku200@gmail.com>
2026-09-21 15:53:07 -0700

    added another kata

---
+++ VolumeofaCuboid.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a
+// kata: Volume of a Cuboid
+// My solution:
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    // your code here
+    return length * width * height
+  }
+}
