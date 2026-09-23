9eb361aa04568c421bf9ce4bb35faecb58361108
Njoku360 <frannjoku200@gmail.com>
2026-09-23 14:14:52 -0700

    added another kata

--- Alphabetposition.js
+++ Alphabetposition.js
@@ -4,3 +4,20 @@
 function alphabetPosition(text) {
   return text.toLowerCase().split('').filter(l => l >= 'a' && l <= 'z').map(l => l.charCodeAt(0) - 96).join(' ')
 }
+
+function likes(names) {
+  const count = names.length;
+
+  switch (count) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
---
+++ SumtheStrings.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5966e33c4e686b508700002d
+// kata: Sum the Strings
+// My solution:
+function sumStr(a,b) {
+  let A = Number(a)
+  let B = Number(b)
+  let result = A + B
+  return String(result)
+}
